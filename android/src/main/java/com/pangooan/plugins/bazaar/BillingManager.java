1d8bf184707c9c70d47b08c5330bbb9b553c4e0d
alirezasonboli5 <alirezasonboli5@gmail.com>
2025-04-01 14:02:47 +0330

    start

--- android/src/main/java/com/pangooan/plugins/bazaar/BillingManager.java
+++ android/src/main/java/com/pangooan/plugins/bazaar/BillingManager.java
@@ -1,11 +1,46 @@
 package com.pangooan.plugins.bazaar;
 
-import android.util.Log;
+import android.content.Context;
+import ir.cafebazaar.poolakey.Payment;
+import ir.cafebazaar.poolakey.config.PaymentConfiguration;
+import ir.cafebazaar.poolakey.config.SecurityCheck;
+import ir.cafebazaar.poolakey.callback.ConnectionCallback;
 
 public class BillingManager {
 
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
+    private Payment payment;
+
+    public BillingManager(Context context, String rsaPublicKey) {
+        SecurityCheck securityCheck;
+        if (rsaPublicKey == null || rsaPublicKey.isEmpty()) {
+            securityCheck = SecurityCheck.Disable;
+        } else {
+            securityCheck = SecurityCheck.Enable(rsaPublicKey);
+        }
+        PaymentConfiguration config = new PaymentConfiguration(securityCheck);
+        payment = new Payment(context, config);
+    }
+
+    public void connect(final Runnable onSuccess, final java.util.function.Consumer<Throwable> onError) {
+        payment.connect(new ConnectionCallback() {
+            @Override
+            public void connectionSucceed() {
+                onSuccess.run();
+            }
+
+            @Override
+            public void connectionFailed(Throwable error) {
+                onError.accept(error);
+            }
+
+            @Override
+            public void disconnected() {
+                onError.accept(new Exception("Disconnected"));
+            }
+        });
+    }
+
+    public void disconnect() {
+        payment.disconnect();
     }
 }
--- android/src/main/java/com/pangooan/plugins/bazaar/BillingManagerPlugin.java
+++ android/src/main/java/com/pangooan/plugins/bazaar/BillingManagerPlugin.java
@@ -3,20 +3,42 @@ package com.pangooan.plugins.bazaar;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.PluginMethod;
+import android.content.Context;
 
-@CapacitorPlugin(name = "BillingManager")
+@CapacitorPlugin(name = "CafeBazaarBilling")
 public class BillingManagerPlugin extends Plugin {
 
-    private BillingManager implementation = new BillingManager();
+    private BillingManager billingManager;
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void initialize(final PluginCall call) {
+        String rsaPublicKey = call.getString("rsaPublicKey");
+        Context context = getContext();
+        billingManager = new BillingManager(context, rsaPublicKey);
+        billingManager.connect(new Runnable() {
+            @Override
+            public void run() {
+                JSObject ret = new JSObject();
+                ret.put("message", "Connected successfully");
+                call.resolve(ret);
+            }
+        }, new java.util.function.Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable error) {
+                call.reject("Connection failed: " + error.getMessage());
+            }
+        });
+    }
 
+    @PluginMethod
+    public void disconnect(PluginCall call) {
+        if (billingManager != null) {
+            billingManager.disconnect();
+        }
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        ret.put("message", "Disconnected");
         call.resolve(ret);
     }
 }
